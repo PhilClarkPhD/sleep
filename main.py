711c97c1aae033ef1d38eda37ff2bf959d28c546
Phil Clark <phil.clark.phd@gmail.com>
2024-05-10 23:13:48 -0400

    Added main.py, windows.py, utils.py
    add: main.py - attempt at rebuilding Mora.py. Runs main application and calls windows.py for home and model tabs
    add window.py - contains the layout for home and model tabs. contains Tab class with home() and model() functions. Creates instance of Display class
    add utils.py defines the Display() class with intial values for all display objects in the home window.

---
+++ main.py
@@ -0,0 +1,38 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
+import os
+import windows
+
+START_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        central_widget = QWidget()  # Create a central widget
+        self.setCentralWidget(
+            central_widget
+        )  # Set it as the central widget of the main window
+
+        layout = QVBoxLayout(central_widget)  # Create a layout for the central widget
+
+        tabs = QTabWidget()
+        tabs.addTab(windows.Tab().home(), "home")
+        tabs.addTab(windows.Tab().model(), "Model")
+
+        layout.addWidget(tabs)  # Add the tabs widget to the layout
+
+
+def run():
+    app = QApplication([])
+    GUI = MainWindow()
+    GUI.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    run()
