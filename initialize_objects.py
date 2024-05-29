95d3e282c12c4a90de2b1a09ce724f5acd6589f3
Phil Clark <phil.clark.phd@gmail.com>
2024-05-28 23:18:53 -0400

    FIRST FUNCTIONAL REFACTOR. Mora.py has been split into 4 modules that define different classes. Each module creates an instance of a class from the module below it in the following order: - main.py: Builds and runs the QApplication. Creates instance of Tabs class - tabs.py: Defines the Tabs class which defines home_tab() and model_tab() w/layouts for buttons, labels, and plots. Connects buttons to functions in the Funcs class. - update_objects.py: Defines the Funcs class which houses all methods for updating data/objects/plots within the app. Creates an instance of the General, Data, Home, and Model classes- initialize_objects.py: Defines the General, Data, Home, and Model classes. Create initialized values of all QButtons, QLabels, plots and data variables (epoch, epoch_dict, df, etc.)

--- initialize_objects.py
+++ initialize_objects.py
@@ -1,39 +1,75 @@
-"""
-TODO:
-Clean up:
--remove all slot function references (the funcs should go to update_objects.py, the calls to them should be made by
-tabs.py
-- double check that the way things are defined makes sense (e.g. hard-coding things may not be ideal)
-"""
-
 from PyQt5.QtWidgets import *
 import pyqtgraph as pg
+import os
+import numpy as np
+import pandas as pd
 
 
-class Display(QWidget):
+class General(QWidget):
     def __init__(self):
         super().__init__()
 
-        # Data Objects, labels, buttons
-        # Epoch
+        # PATH
+        self.current_path = os.path.dirname(os.path.realpath(__file__))
+
+
+class Data(QWidget):
+    def __init__(self):
+        super().__init__()
+
         self.epoch = 0
+        self.epoch_dict = {}
+        self.epoch_list = []
 
-        # epoch label
-        self.epoch_win = QLabel("Epoch: {} ".format(self.epoch), self)
+        self.df = pd.DataFrame(columns=["eeg", "emg"])
+        self.metrics = pd.DataFrame(
+            columns=["delta_rel", "theta_rel", "theta_over_delta"]
+        )
 
-        # current file label
-        self.file_win = QLabel("", self)
+        self.samplerate = np.NaN
+        self.array_size = np.NaN
 
-        # current score label
-        self.score_win = QLabel("", self)
+        self.window_start = np.NaN
+        self.window_end = np.NaN
+
+        self.eeg_power = {}
+        self.emg_power = {}
+
+        self.wake_count = 0
+        self.nrem_count = 0
+        self.rem_count = 0
+
+
+class Home(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # # # TOP LAYOUT # # #
+
+        # Current epoch label
+        self.current_epoch_label = QLabel("Epoch: ", self)
+
+        # Current file label
+        self.current_wav_file_label = QLabel("File: ", self)
+
+        # Current score label
+        self.current_score_file_label = QLabel("Scores: ", self)
 
         # create plot objects in window
-        self.window_size = QComboBox(self)
+        self.window_size_dropdown = QComboBox(self)
+        self.window_size_dropdown.addItems(["1", "3", "5", "7"])
+        self.window_size_dropdown.setCurrentIndex(2)
 
         # Buttons
         self.load_data_button = QPushButton("Load Data", self)
-        self.load_data_button.clicked.connect(self.Buttons.load_data)
+        self.load_scores_button = QPushButton("Load Scores", self)
+        self.clear_scores_button = QPushButton("Clear Scores", self)
+        self.export_scores_button = QPushButton("Export Scores", self)
+        self.export_breakdown_button = QPushButton("Export Breakdown", self)
+        self.find_epoch_button = QPushButton("Find epoch", self)
+        self.next_REM_button = QPushButton("Next REM", self)
 
+        # # # MIDDLE LAYOUT # # #
         # EEG plot
         self.eeg_plot = pg.PlotWidget(self, title="EEG")
         self.line1 = pg.InfiniteLine(
@@ -62,5 +98,23 @@ class Display(QWidget):
         self.hyp_labels = {0: "Wake", 1: "Non REM", 2: "REM", 3: "Unscored"}
         (self.hypnogram.getAxis("left")).setTicks([self.hyp_labels.items()])
         self.hypno_line = pg.InfiniteLine(
-            pos=self.epoch, movable=True, angle=90, pen=pg.mkPen(width=3, color="r")
+            pos=0, movable=True, angle=90, pen=pg.mkPen(width=3, color="r")
         )
+
+
+class Model(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Model labels
+        self.current_model_label = QLabel("Current Model Version: None", self)
+        self.scoring_complete_label = QLabel("", self)
+
+        # Buttons
+        self.load_model_button = QPushButton("Load Model", self)
+        self.score_data_button = QPushButton("Score Data", self)
+
+        # Model Objects
+        self.model = np.NaN
+        self.params = {}
+        self.label_encoder = {}
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 import sys
-import os
 from tabs import Tabs
+import pyqtgraph as pg
+from update_objects import Funcs
+
+pg.setConfigOption("background", "w")
+pg.setConfigOption("foreground", "k")
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.setGeometry(50, 50, 1125, 525)
+        self.setWindowTitle("Mora Sleep Analysis")
+        self.setStyleSheet("font-size: 20px;")
 
+        self.Funcs = Funcs()
         self.Tabs = Tabs()
 
-        central_widget = QWidget()  # Create a central widget
-        self.setCentralWidget(
-            central_widget
-        )  # Set it as the central widget of the main window
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-        layout = QVBoxLayout(central_widget)  # Create a layout for the central widget
+        layout = QVBoxLayout(central_widget)
 
         tabs = QTabWidget()
-        tabs.addTab(self.Tabs.test_tab(), "test")
-        # tabs.addTab(Tabs.home(), "home")
-        # tabs.addTab(Tabs.model(), "Model")
+        tabs.addTab(self.Tabs.home_tab(), "Home")
+        tabs.addTab(self.Tabs.model_tab(), "Model")
 
-        layout.addWidget(tabs)  # Add the tabs widget to the layout
+        layout.addWidget(tabs)
+
+    def keyPressEvent(self, event):  # Needed to ensure key press functionality
+        self.Tabs.Funcs.keyPressEvent(event)
 
 
 def run():
