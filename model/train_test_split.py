97df71d50fe15f8ca2b3782e8ebe9451dfe461cd
Phil Clark <Phil.Clark.PhD@gmail.com>
2024-06-30 15:54:23 -0400

    FIX/ADD
    FIX: train_test_split.py was incorrectly peforming train_test_split and duplicating data, refactored function to fix it
    ADD: Notebook investigating class imbalance in train and test set following conversation w/ Sophie and Rodrigo.

--- model/train_test_split.py
+++ model/train_test_split.py
@@ -13,13 +13,11 @@ def train_test_split(
 
     Args:
         df (pd.DataFrame):  A pandas dataframe with the data, group var, and some time-series indicator.
-        feature_cols (list): List of columns to retain as predictors.
         train_size (float): Proportion (between 0 and 1) of data to ues in the training set. Test set will be the
         remainder. Defaults to 0.8.
         time_series_idx (str): Column name to use to order rows prior to splitting train and test. Defaults to 'epoch'.
         group_col (str): Column name to use for grouping data to ensure equal sampling across subjects. Defaults to
         'ID_day'.
-        target_col (str): Column name of variable that is target of model predictions. Default value is 'score'.
 
     Returns:
         (pd.DataFrame): training data.
@@ -29,24 +27,18 @@ def train_test_split(
     # First enforce correct order of time_col by sorting the values within each group of group_col
     df = df.sort_values(by=[group_col, time_series_idx])
 
-    # Identify the group sizes
-    group_sizes = df.groupby(group_col).size()
+    train_df = pd.DataFrame()
+    test_df = pd.DataFrame()
 
-    # Initialize empty lists to store training and testing indices
-    train_indices = []
-    test_indices = []
+    for group in df[group_col].unique():
+        group_df = df.loc[df[group_col] == group]
+        total_rows = len(group_df)
+        train_rows = total_rows * train_size
 
-    for group, size in group_sizes.items():
-        # Calculate the number of rows for training and testing by group
-        n_train_rows = int(train_size * size)
+        train_set = group_df.loc[:train_rows]
+        test_set = group_df.loc[train_rows:]
 
-        # Get the indices for training and testing rows
-        group_indices = df.index[df[group_col] == group]
-        train_indices.extend(group_indices[:n_train_rows])
-        test_indices.extend(group_indices[n_train_rows:])
+        train_df = pd.concat([train_df, train_set])
+        test_df = pd.concat([test_df, test_set])
 
-    # Extract training and testing datasets
-    train_set = df.loc[train_indices]
-    test_set = df.loc[test_indices]
-
-    return train_set, test_set
+    return train_df, test_df
