8c407f4b15fcbf1208e3ef98c3bfab2b5b365382
Phil Clark <Phil.Clark.PhD@gmail.com>
2024-07-14 11:16:55 -0400

    FIX: changed time_series_idx to time_series_index

--- model/train_test_split.py
+++ model/train_test_split.py
@@ -4,18 +4,18 @@ import pandas as pd
 def train_test_split(
     df: pd.DataFrame,
     train_size: float = 0.8,
-    time_series_idx: str = "epoch",
+    time_series_index: str = "epoch",
     group_col: str = "ID_day",
 ) -> tuple:
     """
     Splits data into train and test sets. Splitting is done equally across all values in the group_col.
-    Splits done in time-series fashion - e.g. both train and test are continuous values wrt the time_series_idx.
+    Splits done in time-series fashion - e.g. both train and test are continuous values wrt the time_series_index.
 
     Args:
         df (pd.DataFrame):  A pandas dataframe with the data, group var, and some time-series indicator.
         train_size (float): Proportion (between 0 and 1) of data to ues in the training set. Test set will be the
         remainder. Defaults to 0.8.
-        time_series_idx (str): Column name to use to order rows prior to splitting train and test. Defaults to 'epoch'.
+        time_series_index (str): Column name to use to order rows prior to splitting train and test. Defaults to 'epoch'.
         group_col (str): Column name to use for grouping data to ensure equal sampling across subjects. Defaults to
         'ID_day'.
 
@@ -25,7 +25,7 @@ def train_test_split(
     """
 
     # First enforce correct order of time_col by sorting the values within each group of group_col
-    df = df.sort_values(by=[group_col, time_series_idx])
+    df = df.sort_values(by=[group_col, time_series_index])
 
     train_df = pd.DataFrame()
     test_df = pd.DataFrame()
